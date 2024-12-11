abc118321efdae145c8e086c6605d5ba4bf9b91b
Renan Borges <renanvcb@gmail.com>
2024-12-11 13:23:08 -0300

    feat: add fonts to app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,26 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
 
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
+
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
   return (
     <Stack
       screenOptions={{
