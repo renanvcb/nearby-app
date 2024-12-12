345a60360ad994b6db33413585bb8cb3a2444800
Renan Borges <renanvcb@gmail.com>
2024-12-12 09:47:11 -0300

    feat: welcome page updated

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,17 +1,20 @@
 import { View, Text } from "react-native";
+import { IconProps } from "@tabler/icons-react-native";
 
 import { colors } from "@/styles/theme";
 import { s } from "./styles";
 
 interface stepProps {
-  icon?: string;
+  icon: React.ComponentType<IconProps>;
   title: string;
   description: string;
 }
 
-export function Step({ icon, title, description }: stepProps) {
+export function Step({ icon: Icon, title, description }: stepProps) {
   return (
     <View style={s.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
+
       <View style={s.details}>
         <Text style={s.title}>{title}</Text>
         <Text style={s.description}>{description}</Text>
--- src/components/steps/index.tsx
+++ src/components/steps/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native";
+import { IconMapPin, IconQrcode, IconTicket } from "@tabler/icons-react-native";
 
 import { Step } from "../step";
 
@@ -9,14 +10,17 @@ export function Steps() {
     <View style={s.container}>
       <Text style={s.title}>Veja como funciona:</Text>
       <Step
+        icon={IconMapPin}
         title="Encontre estabelecimentos"
         description="Veja locais perto de você que são parceiros Nearby"
       />
       <Step
+        icon={IconQrcode}
         title="Ative o cupom com QR Code"
         description="Escaneie o código no estabelecimento para usar o benefício"
       />
       <Step
+        icon={IconTicket}
         title="Garanta vantagens perto de você"
         description="Ative cupons onde estiver, em diferentes tipos de estabelecimento "
       />
