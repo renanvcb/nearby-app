c10469040adf96acc4370eff7dce691908c0d025
Renan Borges <renanvcb@gmail.com>
2024-12-15 16:56:38 -0300

    feat: set a page to sho place details

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,12 +1,43 @@
-import { Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Alert, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+
+import { Loading } from "@/components/loading";
+
+import { api } from "@/services/api";
 
 export default function Market() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const params = useLocalSearchParams<{ id: string }>();
 
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Erro",
+        "Não foi possível carregar as informações do local.",
+        [{ text: "OK", onPress: () => router.back() }]
+      );
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket();
+  }, [params.id]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>{params.id}</Text>
-    </View>
+    <View
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    ></View>
   );
 }
