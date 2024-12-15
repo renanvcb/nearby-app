ae9222262987cbd4bbe259cfceb117a313d30015
Renan Borges <renanvcb@gmail.com>
2024-12-15 18:41:08 -0300

    feat: market details screen almost finished

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
-import { Alert, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { Alert, Modal, View } from "react-native";
+import { router, useLocalSearchParams, Redirect } from "expo-router";
+import { IconScan } from "@tabler/icons-react-native";
 
 import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+import { Details, PropsDetails } from "@/components/market/details";
+import { Coupon } from "@/components/market/coupon";
+import { Button } from "@/components/button";
 
 import { api } from "@/services/api";
 
+interface DataProps extends PropsDetails {
+  cover: string;
+}
+
 export default function Market() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<DataProps>();
   const [isLoading, setIsLoading] = useState(true);
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
 
   const params = useLocalSearchParams<{ id: string }>();
 
@@ -27,6 +37,14 @@ export default function Market() {
     }
   }
 
+  function handleCamera() {
+    try {
+      setIsVisibleCameraModal(!isVisibleCameraModal);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     fetchMarket();
   }, [params.id]);
@@ -35,9 +53,27 @@ export default function Market() {
     return <Loading />;
   }
 
+  if (!data) {
+    return <Redirect href="/home" />;
+  }
+
   return (
-    <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    ></View>
+    <View style={{ flex: 1 }}>
+      <Cover uri={data.cover} />
+      <Details data={data} />
+      {data.coupons > 0 && <Coupon code="FM345T6" />}
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleCamera}>
+          <Button.Icon icon={IconScan} />
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <Button onPress={handleCamera}>
+          <Button.Title>Voltar</Button.Title>
+        </Button>
+      </Modal>
+    </View>
   );
 }
